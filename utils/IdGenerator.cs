212cd24a23e7d77f7da4b3abb0642f967d7c0dbd
paulo.guerra <paulo.guerra@flex-am.com.br>
2025-04-23 08:26:45 -0400

    refactor: id generator method

---
+++ utils/IdGenerator.cs
@@ -0,0 +1,13 @@
+﻿namespace BooksManagement.Utils;
+
+public class IdGenerator
+{
+    private static int _id = 0;
+
+    public static int GenerateId()
+    {
+        _id++;
+
+        return _id;
+    }
+}
